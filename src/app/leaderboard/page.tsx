78e6b2696fd66cacb4ce7e96e815845278edb245
Mansoor Barri <mansoor.eb.ak@gmail.com>
2024-11-07 12:30:31 +0000

    add: /leaderboard

---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Leaderboard() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Leaderboard</h1>
+    </div>
+  );
+}
