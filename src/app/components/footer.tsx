44372223a1c028ac33ba37fa7311657afcb017e8
Mansoor Barri <mansoor.eb.ak@gmail.com>
2024-11-24 00:34:07 +0000

    update: include college website in the footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,9 @@ export default function Footer() {
     return (
         <footer className="bg-black text-white text-center text-sm">    
             <p className="text-white text-center">
-                Cardinal Newman College
+                <a href="https://www.cardinalnewman.ac.uk/">
+                    Cardinal Newman College
+                </a>
             </p>
         </footer>
     )
